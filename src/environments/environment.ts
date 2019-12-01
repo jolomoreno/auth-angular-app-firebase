3368697f6cd14dff717400b401fed80eb16196b9
Jose Lorenzo Moreno <jolorenzom@gmail.com>
2019-12-01 22:17:23 +0100

    Git clone and set firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+    apiKey: 'AIzaSyBSSPia7EULBY3DDr70Cik2QmwgJhXqS_A',
+    authDomain: 'auth-angular-app-firebase.firebaseapp.com',
+    databaseURL: 'https://auth-angular-app-firebase.firebaseio.com',
+    projectId: 'auth-angular-app-firebase',
+    storageBucket: 'auth-angular-app-firebase.appspot.com',
+    messagingSenderId: '422048308446',
+    appId: '1:422048308446:web:3053addb666aac9051364f'
   }
 };
 
